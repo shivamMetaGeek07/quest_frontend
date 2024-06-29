aa7f5465e422c60f4483fbca3edfced00bdd6432
hariom-verma-meta <hariom.verma@metageeks.tech>
2024-06-29 17:30:24 +0530

    asked are done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
-import Community from "./component/Community";
+import Community from "./components/Community";
 
-export default function Home() {
+export default function Home ()
+{
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       landing page
       <div>
-        <Community/>
+        <Community />
       </div>
     </main>
   );
