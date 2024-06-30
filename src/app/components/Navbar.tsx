fb96674041c8c0820b4e519f544479f317836c7a
shivam <shivam@metageeks.tech>
2024-06-30 13:20:32 +0530

    login Link changed

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -168,7 +168,7 @@ const Navbar = () => {
             </li>
             <li>
               <Link
-                href='/login'
+                href='/user/login'
                 className='block py-2 px-3 text-white bg-cyan-700 rounded md:bg-transparent md:text-cyan-700 md:p-0 md:dark:text-cyan-500'
                 aria-current='page'
               >
@@ -177,7 +177,7 @@ const Navbar = () => {
             </li>
             <li>
               <Link
-                href='/signup'
+                href='/user/signup'
                 className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-cyan-700 md:p-0 md:dark:hover:text-cyan-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
               >
                 Signup
