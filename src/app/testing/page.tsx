d75a4e52c026c476b5d44f263c66c2776e5c78f2
shivam <shivam@metageeks.tech>
2024-07-10 18:34:45 +0530

    testing

---
+++ src/app/testing/page.tsx
@@ -0,0 +1,14 @@
+import NavBartest from "../components/testing/navbar";
+
+const testing=()=>{
+
+    return(
+        <div>
+        
+        <NavBartest/>
+
+        </div>
+    );
+}
+
+export default testing;
