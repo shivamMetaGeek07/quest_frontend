aeea62700be3b7801c2e7e5b572791836816ac40
shivam <shivam@metageeks.tech>
2024-07-17 18:16:55 +0530

    telegram page added

--- src/app/telegram/page.tsx
+++ src/app/telegram/page.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
     script.async = true;
     script.setAttribute('data-telegram-login', 'Anijojo_bot');
     script.setAttribute('data-size', 'large');
-    script.setAttribute('data-onauth', 'onTelegramAuth(user)');
+    script.setAttribute('data-auth-url', 'https://docsblock.io/auth/telegram/callback');
     script.setAttribute('data-request-access', 'write');
     document.getElementById('telegram-login')?.appendChild(script);
     (window as any).onTelegramAuth = onTelegramAuth;
@@ -17,7 +17,7 @@ const App: React.FC = () => {
   const onTelegramAuth = (user: any) => {
     alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
     // Send user data to your backend for verification and further processing  
-    fetch('http://localhost:8080/auth/telegram/callback', {
+    fetch('https://docsblock.io/auth/telegram/callback', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -25,22 +25,18 @@ const App: React.FC = () => {
       body: JSON.stringify(user)
     }).then(response => {
       // Handle response from the server
-      console.log("res",response);
+      console.log("res", response);
     }).catch(error => {
       console.error('Error:', error);
     });
   };
-  
-  
-  
+
   return (
     <div className="App flex justify-center w-full m-auto items-center">
       <h1>Login with Telegram</h1>
       <div id="telegram-login"></div>
-      <button onClick={onTelegramAuth}>send</button>
     </div>
   );
 };
 
 export default App;
-
