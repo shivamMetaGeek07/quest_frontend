17a0becb08bd66e965a8b5af3c89d372b91f4b78
shivam <shivam@metageeks.tech>
2024-07-23 17:40:56 +0530

    telegram testing

--- src/app/telegram/page.tsx
+++ src/app/telegram/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useEffect } from 'react';
-
+import { useRouter } from 'next/navigation';
 const TeleApp: React.FC = () => {
   // useEffect(() => {
   //   const script = document.createElement('script');
@@ -104,6 +104,28 @@ const TeleApp: React.FC = () => {
       window.removeEventListener('telegram-login', handleTelegramLogin);
     };
   }, []);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = async () => {
+      const urlParams = new URLSearchParams(window.location.search);
+      const telegramData = {
+        id: urlParams.get('id'),
+        first_name: urlParams.get('first_name'),
+        last_name: urlParams.get('last_name'),
+        username: urlParams.get('username'),
+        photo_url: urlParams.get('photo_url'),
+        auth_date: urlParams.get('auth_date'),
+        hash: urlParams.get('hash'),
+      };
+
+      console.log("telegramData",telegramData);
+    };
+
+    // Handle initial route change
+    handleRouteChange();
+  }, [router]);
 
   // Define your custom function here
   const yourCustomFunction = (user: any) => {
