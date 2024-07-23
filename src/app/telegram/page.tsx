8fb966b329c7b443b6fcffc414fba61cb27f58c5
DiwanshGupta <dewanshgupta120@gmail.com>
2024-07-23 13:06:18 +0530

    fix telegram ui

--- src/app/telegram/page.tsx
+++ src/app/telegram/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useEffect } from 'react';
 
-const App: React.FC = () => {
+const TeleApp: React.FC = () => {
   useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://telegram.org/js/telegram-widget.js?22';
@@ -14,11 +14,10 @@ const App: React.FC = () => {
   }, []);
 
   return (
-    <div className="App flex justify-center w-full m-auto items-center">
-      <h1>Login with Telegram</h1>
+    <div className="App flex">
       <div id="telegram-login"></div>
     </div>
   );
 };
 
-export default App;
+export default TeleApp;
