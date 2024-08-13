0de1dae57e8772663709044aea5c1a7d63e78291
sajidmeta <sajid@metageeks.tech>
2024-08-13 12:04:05 +0530

    landing page

--- src/app/ecosystem/[slug]/page.tsx
+++ src/app/ecosystem/[slug]/page.tsx
@@ -35,7 +35,7 @@ const Page = ( params: { params: { slug: any; }; } ) =>
 
 
     return (
-        <div className='bg-[#111111] pt-[5rem] font-[ProFontWindows]'>
+        <div className='pt-[5rem] font-[ProFontWindows]'>
             <div className='flex-col  lg:flex-row items-center flex justify-between'>
                 <div className='flex flex-row text-xl    items-center justify-around m-auto'>
                     <div className=' p-3 '>
@@ -58,7 +58,7 @@ const Page = ( params: { params: { slug: any; }; } ) =>
 
 
                 </div>
-                <div className='flex flex-row text-gray-500 text-xl w-[36rem] justify-between h-32 bg-[#111111] shadow-lg shadow-[#0d0d0d]  items-center  m-auto'>
+                <div className='flex flex-row text-gray-500 text-xl w-[36rem] justify-between h-32  shadow-lg shadow-[#0d0d0d]  items-center  m-auto'>
                     <div
                         className='bg_Div_half h-[8rem] w-[16rem] items-center flex'>
                         <img src='https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg' alt='' className='styled-image ' />
@@ -85,45 +85,82 @@ const Page = ( params: { params: { slug: any; }; } ) =>
 
                 </div>
             </div>
-            <hr className="h-[1px] my-2 items-center m-auto border-[0.5px] w-3/4 border-dashed bg-violet-800 " />
+             <div className='mx-24 mt-4'>
+             <svg xmlns="http://www.w3.org/2000/svg" width="1082" height="2" viewBox="0 0 1082 2" fill="none">
+  <path opacity="0.7" d="M1081 1L1.00001 1" stroke="url(#paint0_linear_148_4119)" stroke-linecap="round" stroke-dasharray="13 13"/>
+  <defs>
+    <linearGradient id="paint0_linear_148_4119" x1="1081" y1="0.5" x2="1" y2="0.5" gradientUnits="userSpaceOnUse">
+      <stop stop-color="#8C71FF" stop-opacity="0"/>
+      <stop offset="1" stop-color="#FA00FF"/>
+    </linearGradient>
+  </defs>
+</svg>
+             </div>
 
             {/* map the ecosytem */ }
-            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-7'>
+            <div className='mx-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-7'>
                 { data?.map( ( item: any, index: number ) => (
-                    <div key={ index } className='outer-div flex flex-col border border-[#282828] hover:bg-[#8c71ff] hover:text-[#111111] p-4 rounded-lg transition-colors duration-300 w-full max-w-[22rem] mx-auto'>
-                        <div className='flex flex-col sm:flex-row items-center justify-between mb-4'>
-                            <div className='mb-4 sm:mb-0'>
-                                <div className='image-container h-20 w-20 flex items-center justify-center mb-2'>
-                                    <img src={ item.logo } alt={ item.title } className='styled-image max-w-full max-h-full' />
-                                </div>
-                                <div className='bg_Div_Down h-8 bg-gray-800 w-full' />
-                            </div>
-                            <div className='flex flex-col w-full sm:w-auto'>
-                                <div className='bg_eco_div border-b-4 border-[#8c71ff] bg-[#1d1a28] p-4 rounded-t-lg w-full'>
-                                    <div className='text-lg mb-2'>{ item.title }</div>
-                                    <div className='text-xs flex justify-between'>
-                                        <div className='flex flex-col items-center'>
-                                            <span>{ item?.quests?.length }</span>
-                                            <span>Quests</span>
-                                        </div>
-                                        <div className='flex flex-col items-center'>
-                                            <span>{ item?.members?.length }</span>
-                                            <span>Followers</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className='flex justify-end gap-1 mt-2'>
-                                    <div className='eco_box w-5 h-5 bg-[#8c71ff]' />
-                                    <div className='eco_box w-5 h-5 bg-[#8c71ff]' />
-                                    <div className='eco_box w-5 h-5 bg-[#8c71ff]' />
-                                </div>
-                            </div>
-                        </div>
-                        <div className='text-sm'>
-                            <span className='font-semibold descText'>Desc:</span>
-                            <p className='text-gray-600 mt-1 descdata'>{ item.description }</p>
-                        </div>
-                    </div>
+ <div key={ index } className="outer-div relative flex lg:gap-2 sm:gap-4 gap-4  hover:bg-[#8c71ff] hover:text-[#111111] border-[#282828] border rounded-md lg:p-5 sm:p-2 p-4 flex-col justify-center w-full sm:w-full ">
+   <div className="flex flex-row text-xl items-center justify-around ">
+  <div className="p-1">
+    <div className="image-container md:h-[5rem] md:w-[5rem] h-[4rem] w-[4rem] items-center flex">
+      <img src={ item.logo } alt={ item.title }  className="styled-image" />
+    </div>
+    <div className="bg_Div_Down h-[2rem] mt-2 bg-gray-800" />
+  </div>
+
+  <div className="md:w-2/3 w-2/3 flex flex-col justify-start gap-2 ">
+    <div className="flex w-full flex-col items-start ">
+      <div className="flex w-full md:h-[5rem] bg_eco_div border-b-4 border-[#8c71ff] gap-2 md:gap-2  p-2 bg-[#28223d] flex-col lg:flex-row items-center md:items-end lg:items-end justify-between ">
+        <div className="md:w-4/5  w-4/5 truncate text-[12px] md:text-[10px] lg:text-[10px] md:ml-3 md:text-start text-center card-title">
+          {item.title}
+        </div>
+
+        <div className="md:1/5 flex flex-row rounded-lg justify-center md:justify-end">
+          <div className="flex gap-1 mr-2 items-center flex-col">
+            <span className="card-white-text text text-lg ">
+            { item?.quests?.length }
+            </span>
+            <span className=" card-gray-text text-3xl">QUESTS</span>
+          </div>
+          <div className="flex gap-1 items-center flex-col">
+            <span className="card-white-text text-lg">
+            { item?.members?.length }
+            </span>
+            <span className=" card-gray-text text-lg">FOLLOWERS</span>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+<div className="absolute -top-1 -right-1">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="6"
+    height="6"
+    viewBox="0 0 4 4"
+    fill="none"
+  >
+    <path d="M0.5 0V3.5H4" stroke="white" />
+  </svg>
+</div>
+
+<div>
+  <div className="flex flex-row text-xs m-1 gap-2 justify-start">
+    <span className="descText">Bio:</span>
+    <span className="descdata text-wrap break-words overflow-hidden line-clamp-2">
+    { item.description }
+    </span>
+  </div>
+</div>
+</div> 
+
+
+
+
                 ) ) }
             </div>
         </div>
