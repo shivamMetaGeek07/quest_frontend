c82fd100d2d20e0dd53461e6d0203b5debddc6af
diwanshMeta <diwansh@metageeks.tech>
2024-08-19 14:08:51 +0530

    teleram fix

--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -147,7 +147,6 @@ const Profile: React.FC = () =>
   //       },
   //       withCredentials: true,
   //     });
-  
   //     console.log(response.data);
   //   } catch (error) {
   //     console.error('Error checking if user liked the tweet:', error);
@@ -175,14 +174,6 @@ const Profile: React.FC = () =>
   // };
   useEffect( () =>
   {
-      const script = document.createElement('script');
-      script.src = 'https://telegram.org/js/telegram-widget.js?22';
-      script.async = true;
-      script.setAttribute('data-telegram-login', 'Anijojo_bot');  // Your Bot Username
-      script.setAttribute('data-size', 'medium');  // Widget Size: small, medium, large
-      script.setAttribute('data-auth-url', 'https://docsblock.io/auth/telegram/callback');  // Back-End Callback URL
-      script.setAttribute('data-request-access', 'write');  // Permissions requested from the user
-      document.getElementById('telegram-login')?.appendChild(script);
     setIsClient( true );
     dispatch( fetchUserData() );
   }, [] );
